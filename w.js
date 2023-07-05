6422ba87632bae1c3431bd7753e3b5f2ee2f9143
P-1000 <pavan.patchikarla@yahoo.com>
2023-07-05 23:43:14 +0530

    Default commit message

--- w.js
+++ w.js
@@ -1,42 +1,58 @@
-const btn = document.getElementById("but")
-const loc = document.querySelector(".info .location")
-const temp = document.querySelector(".box .info .temp");
+// Select elements from the HTML
+const loc = document.querySelector(".info .location");
+const tempElement = document.querySelector(".box .info .temp");
 
-
-let latitude,longitude;
-if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      latitude = position.coords.latitude;
-      longitude = position.coords.longitude;
-     console.log("Latitude: " + latitude + "\nLongitude: " + longitude);
-    });
-  } else{
-    console.log("Geolocation is not supported by this browser.");
-  }
-
-
-const handleSearch = async ()=>{
-        try {
-               const keywe = '4a440ed568b8454c7f5fd1cc451f6ca5'
-               const wapi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${keywe}`;
-               const response = await fetch(wapi);
-               const data = await response.json();
-              console.log(data)
-                let {temp_max,temp_min,temp ,} = data.main
-              loc.innerHTML=data.name;
-                const celsius =  kTc(temp);
-                temp.innerHTML=temp_max
-                
-        } catch (error) {
-            console.log(error)
-            console.log("error occured")
-            console.log("Geolocation is not supported by this browser.");
-        }
-
-}
-function kTc(kelvin){
-  let celsius = kelvin-273.15;
+// Function to convert Kelvin to Celsius
+function kTc(kelvin) {
+  let celsius = kelvin - 273.15;
   return Math.ceil(celsius);
 }
-btn.addEventListener('click',handleSearch)
- 
+
+// Weather fetching function
+const handleSearch = async () => {
+  try {
+    const keywe = "4a440ed568b8454c7f5fd1cc451f6ca5";
+
+    // Check if latitude and longitude are available
+    if (latitude !== undefined && longitude !== undefined) {
+      const wapi = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${keywe}`;
+
+      // Fetch weather data from the OpenWeatherMap API
+      const response = await fetch(wapi);
+      const data = await response.json();
+      console.log(data);
+
+      // Extract temperature data from the API response
+      let { temp_max, temp_min, temp } = data.main;
+
+      // Display location name
+      loc.innerHTML = data.name;
+
+      // Convert temperature from Kelvin to Celsius
+      const celsius = kTc(temp);
+
+      // Display the maximum temperature
+      tempElement.innerHTML = celsius;
+    } else {
+      console.log("Latitude and longitude data is not available.");
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("An error occurred.");
+  }
+};
+
+// Check if geolocation is supported by the browser
+if (navigator.geolocation) {
+  // Get the user's current position
+  navigator.geolocation.getCurrentPosition(function (position) {
+    latitude = position.coords.latitude;
+    longitude = position.coords.longitude;
+    console.log("Latitude: " + latitude + "\nLongitude: " + longitude);
+    setTimeout(() => {
+      handleSearch();
+    }, 100);
+  });
+} else {
+  console.log("Geolocation is not supported by this browser.");
+}
